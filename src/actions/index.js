e4ee28c6b9dea4f33e5e9f22fcde0b34a38bbaa8
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-11-06 09:55:07 -0700

    Add posting order to backend displaying responce

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,10 @@ export const setOrders = orders => ({
   type: 'SET_ORDERS',
   orders
 });
+
+export const addOrder = order => {
+  return ({
+    type: 'ADD_ORDER',
+    order
+  })
+}
--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -12,21 +12,21 @@ export class Orders extends Component {
     super();
     this.props = props;
   }
+
   componentDidMount() {
     getOrders()
       .then(data => this.props.setOrders(data.orders))
       .catch(err => console.error('Error fetching:', err));
   }
 
-
   render = () => {
    const orderEls = this.props.orders.map(order => {
     return (
-      <div className="order">
+      <div className="order" key={Math.random()}>
         <h3>{order.name}</h3>
         <ul className="ingredient-list">
           {order.ingredients.map(ingredient => {
-            return <li>{ingredient}</li>
+            return <li key={Math.random()}>{ingredient}</li>
           })}
         </ul>
       </div>
