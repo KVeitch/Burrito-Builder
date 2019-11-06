da6c366e530cd33d3e1b267394f277e8c657d1c5
Kirk Veitch <kirk.a.veitch@gmail.com>
2019-11-06 09:15:57 -0700

    Remove store connection from App add connection to Orders

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { setOrders } from '../../actions';
+import { getOrders } from '../../apiCalls';
 import './Orders.css';
 
-const Orders = props => {
-  const orderEls = props.orders.map(order => {
+
+
+export class Orders extends Component {
+  constructor(props) {
+    super();
+    this.props = props;
+  }
+  componentDidMount() {
+    getOrders()
+      .then(data => this.props.setOrders(data.orders))
+      .catch(err => console.error('Error fetching:', err));
+  }
+
+
+  render = () => {
+   const orderEls = this.props.orders.map(order => {
     return (
       <div className="order">
         <h3>{order.name}</h3>
@@ -22,4 +40,15 @@ const Orders = props => {
   )
 }
 
-export default Orders;
+}
+
+export const mapStateToProps = ({ orders }) => ({
+  orders
+});
+
+export const mapDispatchToProps = dispatch => (
+  bindActionCreators({
+    setOrders,
+  }, dispatch)
+);
+export default connect(mapStateToProps, mapDispatchToProps)(Orders);
